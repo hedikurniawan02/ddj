4a555935bb4c7bc2ffd8a4bcc6f4d51bad7de6a7
hedikurniawan02 <142191863+hedikurniawan02@users.noreply.github.com>
2024-01-29 09:32:26 +0700

    Add files via upload

---
+++ X TKJA RIZKI SG KAYU , HEDI SG EVO , ISAL SG AWI GOMBONG/operator_aritmatika.py
@@ -0,0 +1,10 @@
+x = 5
+y = 3
+
+#penjumlahan
+print("Penjumlahan 5 + 3 = ", x+y)
+print("Pengurangan 5 - 3 = ", x-y)
+print("perkalian 5 * 3 = ", x*y)
+print("Pembagian 5 / 3 = ", x/y)
+print("Pembagian bulat 5 // 3 = ", x//y)
+print("perpangkatan 5 ** 3 = ", x**y)
